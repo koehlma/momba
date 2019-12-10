cd1d811cd0ef39ed7e71e2357789f8ce1d450883
Maximilian Köhl <mkoehl@cs.uni-saarland.de>
2019-12-10 19:18:18 +0100

    add model metadata class

---
+++ momba/model/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+#
+# Copyright (C) 2019, Maximilian Köhl <mkoehl@cs.uni-saarland.de>
+
+from __future__ import annotations
+
+import typing as t
+
+import dataclasses
+
+
+@dataclasses.dataclass(frozen=True)
+class Metadata:
+    version: t.Optional[str] = None
+    author: t.Optional[str] = None
+    description: t.Optional[str] = None
+    doi: t.Optional[str] = None
+    url: t.Optional[str] = None
