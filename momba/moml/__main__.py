28835efed20970786753b3a143d467dd64f713b1
Maximilian Köhl <mkoehl@cs.uni-saarland.de>
2020-01-13 16:20:26 +0100

    improve the `moml` tool with `click`

--- momba/moml/__main__.py
+++ momba/moml/__main__.py
@@ -6,34 +6,69 @@ from __future__ import annotations
 
 import typing as t
 
-import argparse
 import pathlib
+import sys
 
-from .parser import TokenStream, parse_moml
+import click
+
+from .parser import TokenStream, parse_moml, MomlSyntaxError
 
 from ..ext.jani import dump_model
 
 
-parser = argparse.ArgumentParser(description="Converts models between MOML and JANI.")
-parser.add_argument("moml_file", type=pathlib.Path)
-parser.add_argument("output_directory", type=pathlib.Path)
-parser.add_argument("--indent", type=int, default=None)
+@click.group()
+def main() -> None:
+    """
+    The *Momba Modeling Language* (MOML) tool.
+    """
 
 
-def main(args: t.Optional[t.Sequence[str]] = None) -> None:
-    namespace = parser.parse_args(args)
+@main.command()
+@click.argument("moml_file", type=pathlib.Path)
+@click.argument("output_directory", type=pathlib.Path)
+@click.option(
+    "--indent", type=int, default=None, help="Indentation to use for the JANI files.",
+)
+@click.option(
+    "--network",
+    "networks",
+    type=str,
+    multiple=True,
+    help="Name of the network(s) to export.",
+)
+def export(
+    moml_file: pathlib.Path,
+    output_directory: pathlib.Path,
+    indent: t.Optional[int] = None,
+    networks: t.Optional[t.Sequence[str]] = None,
+) -> None:
+    """
+    Exports a MOML model to a set of JANI models.
+    """
+    output_directory.mkdir(parents=True, exist_ok=True)
 
-    assert namespace.moml_file.exists()
-
-    namespace.output_directory.mkdir(parents=True, exist_ok=True)
-
-    ctx = parse_moml(TokenStream(namespace.moml_file.read_text(encoding="utf-8")))
+    ctx = parse_moml(TokenStream(moml_file.read_text(encoding="utf-8")))
 
     for network in ctx.networks:
+        if networks and network.name not in networks:
+            continue
         print(f"Exporting network `{network.name}` to JANI...")
-        (namespace.output_directory / f"model_{network.name}.jani").write_bytes(
-            dump_model(network, indent=namespace.indent,)
+        (output_directory / f"model_{network.name}.jani").write_bytes(
+            dump_model(network, indent=indent,)
         )
+
+
+@main.command()
+@click.argument("moml_file", type=pathlib.Path)
+def check(moml_file: pathlib.Path) -> None:
+    """
+    Checks the provided MOML file for validity.
+    """
+    try:
+        parse_moml(TokenStream(moml_file.read_text(encoding="utf-8")))
+    except MomlSyntaxError as error:
+        print(error.args[0])
+        sys.exit(1)
 
 
 if __name__ == "__main__":
