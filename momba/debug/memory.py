9e5996b0b7a8d0d14977697b55d57e64a46c7c7e
Maximilian Köhl <mail@koehlma.de>
2021-12-20 11:31:44 +0000

    chore(debug): improve memory footprint debugger

--- momba/debug/memory.py
+++ momba/debug/memory.py
@@ -11,11 +11,17 @@ import pathlib
 import sys
 import time
 
+from mxu.maps import FrozenMap
+
 
 Classes = t.Union[type, t.Tuple[type, ...]]
 Objects = t.Mapping[type, t.Sequence[t.Any]]
 
 ALL_CLASSES: t.Tuple[type, ...] = (object,)
+
+MAX_DEPTH = 5
+
+_CONTAINERS = (list, tuple, dict, FrozenMap)
 
 
 def fmt_size(
@@ -31,11 +37,11 @@ def fmt_size(
     return result
 
 
-def get_objects(classes: Classes = ALL_CLASSES) -> Objects:
+def get_objects(classes: Classes = ALL_CLASSES, prefix: str = "momba") -> Objects:
     objects: t.Dict[type, t.List[t.Any]] = collections.defaultdict(list)
     for obj in gc.get_objects():
         cls = obj.__class__
-        if cls.__module__.startswith("momba"):
+        if cls.__module__.startswith(prefix):
             if issubclass(cls, classes):
                 objects[obj.__class__].append(obj)
     return objects
@@ -52,29 +58,38 @@ def compute_counts(objects: Objects) -> t.Mapping[type, int]:
     return {cls: len(cls_objects) for cls, cls_objects in objects.items()}
 
 
+def _collect_referrers(obj: t.Any, counts: t.Dict[type, int], depth: int = 0) -> None:
+    for referrer in gc.get_referrers(obj):
+        if depth < MAX_DEPTH and isinstance(referrer, _CONTAINERS):
+            _collect_referrers(referrer, counts, depth + 1)
+        else:
+            counts[referrer.__class__] += 1
+
+
 def compute_referrers(objects: Objects) -> t.Mapping[type, t.Mapping[type, int]]:
     referrers: t.Dict[type, t.Dict[type, int]] = {}
     for cls, cls_objects in objects.items():
-        cls_referrers = referrers[cls] = collections.defaultdict(int)
+        counts = referrers[cls] = collections.defaultdict(int)
         for obj in cls_objects:
-            for referrer in gc.get_referrers(obj):
-                cls_referrers[referrer.__class__] += 1
+            _collect_referrers(obj, counts)
     return referrers
 
 
-def dump_statistics(path: pathlib.Path) -> None:
+def dump_statistics(path: pathlib.Path, prefix: str = "momba") -> None:
     with path.open("at", encoding="utf-8") as file:
         file.write(f"dump_statistics @ {time.monotonic_ns()}\n")
-        objects = get_objects()
+        objects = get_objects(prefix=prefix)
         usage = compute_usage(objects)
         counts = compute_counts(objects)
         referrers = compute_referrers(objects)
         for cls in sorted(objects.keys(), key=lambda cls: usage[cls], reverse=True):
+            cls_name = f"{cls.__module__}.{cls.__name__}"
             file.write(
-                f"  {cls.__name__} | count: {counts[cls]} | size: {fmt_size(usage[cls])}\n"
+                f"  {cls_name} | count: {counts[cls]} | size: {fmt_size(usage[cls])}\n"
                 f"  referrrers: {sum(referrers[cls].values())}\n"
             )
             for ref_cls, ref_count in sorted(
                 referrers[cls].items(), key=lambda item: item[1], reverse=True
             ):
-                file.write(f"    {ref_cls.__name__}: {ref_count}\n")
+                ref_cls_name = f"{ref_cls.__module__}.{ref_cls.__name__}"
+                file.write(f"    {ref_cls_name}: {ref_count}\n")
